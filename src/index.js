dbc0199f26a69b2a593ecc81d0913b0ac084b10f
Frank Lam <frank@duckpun.ch>
2017-05-17 11:46:07 -0400

    annotation support, the triangle

--- src/index.js
+++ src/index.js
@@ -7,7 +7,20 @@ function Stone(props) {
     const {color, radius, x, y} = props;
     const fill = color === go.BLACK ? '#333' : '#fff';
 
-    return <circle cx={x} cy={y} r={radius} fill={fill} filter="url(#shadow)" />;
+    return <circle cx={x} cy={y} r={radius} fill={fill} filter='url(#shadow)' />;
+}
+
+function Annotation({radius, x, y}) {
+    const sideLength = radius * Math.sqrt(3);
+    const dx = sideLength / 2;
+    const dy = Math.sqrt(3) * sideLength / 2 - radius;
+    const points = [
+        [x, y - radius].join(','),
+        [x - dx, y + dy].join(','),
+        [x + dx, y + dy].join(','),
+    ].join(' ');
+
+    return <polygon stroke='#999' points={points} fillOpacity={0}/>;
 }
 
 export class Goban extends Component {
@@ -24,6 +37,7 @@ export class Goban extends Component {
         this.cellSize = this.boardSize / this.dimensions;
         this.startPoint = this.totalMargin + this.cellSize / 2;
         this.stoneSize = this.cellSize / 2 - 3;
+        this.annotationSize = this.stoneSize * 0.8;
     }
 
     boardToSVG(coordinate) {
@@ -66,29 +80,33 @@ export class Goban extends Component {
         return (
             <svg viewBox={svgViewBox} onClick={this.handleClick.bind(this)}>
                 <defs>
-                    <filter id="shadow">
-                        <feDropShadow dx="0.5" dy="0.5" stdDeviation="1" floodColor="#666"/>
+                    <filter id='shadow'>
+                        <feDropShadow dx='0.5' dy='0.5' stdDeviation='1' floodColor='#666'/>
                     </filter>
                 </defs>
 
-                <rect x={this.boardMargin} y={this.boardMargin} fill="#eee" filter="url(#shadow)"
+                <rect x={this.boardMargin} y={this.boardMargin} fill='#eee' filter='url(#shadow)'
                     width={this.viewBox - this.boardMargin * 2} height={this.viewBox - this.boardMargin * 2}/>
 
                 {range(this.dimensions).map(index =>
                     <line key={'x' + index}
                         x1={this.startPoint + index * this.cellSize} x2={this.startPoint + index * this.cellSize}
-                        y1={this.startPoint} y2={this.startPoint + this.boardSize - this.cellSize} stroke="#999"/>
+                        y1={this.startPoint} y2={this.startPoint + this.boardSize - this.cellSize} stroke='#999'/>
                 )}
 
                 {range(this.dimensions).map(index =>
                     <line key={'y' + index}
                         y1={this.startPoint + index * this.cellSize} y2={this.startPoint + index * this.cellSize}
-                        x1={this.startPoint} x2={this.startPoint + this.boardSize - this.cellSize} stroke="#999"/>
+                        x1={this.startPoint} x2={this.startPoint + this.boardSize - this.cellSize} stroke='#999'/>
                 )}
 
                 {this.props.board.moves.map(
                     (color, coordinate) => <Stone color={color} {...this.boardToSVG(coordinate)} radius={this.stoneSize} key={'x:' + coordinate.x + ',y:' + coordinate.y}/>
                 ).valueSeq()}
+
+                {this.props.annotations && this.props.annotations.map(
+                    (coordinate, index) => <Annotation key={'annotation' + index} {...this.boardToSVG(coordinate)} radius={this.annotationSize}/>
+                )}
             </svg>
         );
     }
@@ -97,4 +115,5 @@ export class Goban extends Component {
 Goban.propTypes = {
     board: PropTypes.object.isRequired,
     onCoordinateClick: PropTypes.func,
+    annotations: PropTypes.array,
 }
