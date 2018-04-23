d5f840236cd5753fe510dfbdafdc399be6069355
Frank Lam <frank@duckpun.ch>
2018-04-22 18:24:49 -0700

    update some build deps

--- webpack.config.dev.js
+++ webpack.config.dev.js
@@ -14,7 +14,7 @@ module.exports = {
                 exclude: /node_modules/,
                 loader: 'babel-loader',
                 query: {
-                    presets: ["es2015", "stage-3", "react"]
+                    presets: ["env", "react"]
                 }
             }
         ]
